cc16569b402710a03a451f7455e6e32f2bbd624d
tbnode <tcbucher@gmail.com>
2014-06-14 15:01:48 +0000

    added clientApp and hostApp.js

---
+++ ClassApp/public/javascripts/hostApp.js
@@ -0,0 +1,40 @@
+/*
+ * JavaScript Example
+ * @author: swider
+ */
+
+var App = (function(win, doc, $){
+
+	var
+		// CONFIG
+		containerSel = '#container',
+		buttonSel = '#button',
+
+		// PRIVATE VARIABLES
+		$container,
+		$button,
+
+		// PRIVATE METHODS
+		buttonHandler = function(e) {
+			console.log('Button clicked!');
+			e.preventDefault();
+		},
+
+		// KICK OFF
+		init = function(){
+      console.log('initialized host');
+			$container = $(containerSel);
+			$button = $container.find(buttonSel);
+
+			$button.on('click', buttonHandler);
+		};
+
+	// EXPOSE WHAT YOU NEED
+	return {
+		init: init
+	}
+})(window, document, jQuery);
+
+$(function(){
+  App.init();
+});
